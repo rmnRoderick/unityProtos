6a05f64f271aaaf099646cc9159c4fc0c60e1439
Rodrigo Nieva <rmnRoderick@gmail.com>
2024-02-06 21:28:07 -0300

    Package imported

--- Teleport/Assets/win.cs
+++ Teleport/Assets/win.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 
 public class win : MonoBehaviour
 {
+
+    [SerializeField] private GameObject _winPanel;
+
+    private void Awake()
+    {
+        _winPanel.SetActive(false);
+    }
+
     private void OnTriggerEnter(Collider other)
-    {    
+    {
+        Time.timeScale = 0;
+        _winPanel.SetActive(true);
         Debug.Log("win!");
     }
 
