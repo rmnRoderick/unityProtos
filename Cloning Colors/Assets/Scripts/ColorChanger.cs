0da7778903b3d5bb402c5404cb495363d0731c7c
Rodrigo Nieva <rmnRoderick@gmail.com>
2024-02-06 21:43:53 -0300

    add ColorChanger
    add color selector

--- Cloning Colors/Assets/Scripts/ColorChanger.cs
+++ Cloning Colors/Assets/Scripts/ColorChanger.cs
@@ -5,17 +5,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(SpriteRenderer))]
 public class ColorChanger : MonoBehaviour
 {
+    public enum ColorSet
+    {
+        Red,
+        Blue,
+        Yellow
+    }
+
+    [SerializeField] private ColorSet _currentColor;
+
+    private Dictionary<ColorSet,Color> _colorSetToColor;
+
     private SpriteRenderer mySpriteRenderer;
 
     void Awake()
     {
+        StartColorSetToColor();
         mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void StartColorSetToColor()
+    {
+        _colorSetToColor = new Dictionary<ColorSet, Color>();
+        _colorSetToColor.Add(ColorSet.Red, Color.red);
+        _colorSetToColor.Add(ColorSet.Yellow, Color.yellow);
+        _colorSetToColor.Add(ColorSet.Blue, Color.blue);
     }
 
     private void Start()
     {
-
+        mySpriteRenderer.color = _colorSetToColor[_currentColor];
     }
 }
