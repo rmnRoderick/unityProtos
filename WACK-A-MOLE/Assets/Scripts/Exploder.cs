18ae49dbd3b7efdfac08c7e951d95c03268c186e
Rodrigo Nieva <rmnRoderick@gmail.com>
2024-02-07 21:57:45 -0300

    Add Gotcha
    Add particle system for enemy onTriggerEnter

--- WACK-A-MOLE/Assets/Scripts/Exploder.cs
+++ WACK-A-MOLE/Assets/Scripts/Exploder.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class Exploder : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem _particleSystem;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             print("Aww, you got me");
+            _particleSystem.transform.parent = null;
+            _particleSystem.Play();
             Destroy(gameObject);
-
         }
     }
+
+
 }
